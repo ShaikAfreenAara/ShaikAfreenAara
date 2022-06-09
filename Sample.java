0f8f2edddc6bc86cab83715b7bf1c9aa6ff44370
ShaikAfreenAara <57618017+ShaikAfreenAara@users.noreply.github.com>
2022-06-09 14:39:53 +0530

    Sample

---
+++ Sample.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int a,b;
+    a=sc.nextInt();
+    b=sc.nextInt();
+    System.out.println("Addition of two numbers is:" +(a+b));
+    System.out.println("Difference between two numbers is:" +(a-b));
+    System.out.println("Product of two numbers is:" +(a*b));
+  }
+}
+   
+    
